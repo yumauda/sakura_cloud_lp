9deb3c2a8b725217a24989d6980715e46ccdc2de
yumauda <firststep-yumauda@gmail.com>
2026-02-06 15:58:02 +0900

    タグの修正とOGP追加

--- js/script.js
+++ js/script.js
@@ -15,36 +15,4 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     }
   });
 
-  // // ボタンをクリックしたらスクロールして上に戻る
-  // topBtn.click(function () {
-  //   $('body,html').animate({
-  //     scrollTop: 0
-  //   }, 300, 'swing');
-  //   return false;
-  // });
-
-
-
-
-  // スムーススクロール
-  // - 同一ページ内の #hash のみ対象
-  // - 該当IDがある時だけ preventDefault してスクロール
-  // $(document).on('click', 'a[href*="#"]', function (e) {
-  //   const hash = this.hash;
-  //   if (!hash || hash === '#') return;
-
-  //   // 同一ページ以外（外部URL+hash）は対象外
-  //   if (this.pathname && this.pathname !== location.pathname) return;
-
-  //   const $target = $(hash);
-  //   if (!$target.length) return;
-
-  //   e.preventDefault();
-
-  //   const time = 400;
-  //   const headerH = $('.p-header').outerHeight() ?? $('header').outerHeight() ?? 0;
-  //   const targetY = $target.offset().top - headerH;
-  //   $('html,body').animate({ scrollTop: targetY }, time, 'swing');
-  // });
-
 });
